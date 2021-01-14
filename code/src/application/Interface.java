cf39bfdf7357344175d77108cfd038507ebcb5b8
BlackDIamond-UX <rouiha594@gmail.com>
2021-01-14 10:42:11 +0100

    deleted files

--- code/src/application/Interface.java
+++
@@ -1,8 +0,0 @@
-package application;
-
-import java.util.ArrayList;
-
-public interface Interface {
-	public ArrayList<Apprenant> listeApprenant = new ArrayList<Apprenant>();
-	public ArrayList<Competence> listeCompetence = new ArrayList<Competence>();
-}
